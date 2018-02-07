2784ec7d519628d58b12e81b55b1c92b8c617187
gokulakannan123 <35687945+gokulakannan123@users.noreply.github.com>
2018-02-07 15:12:58 +0530

    Create 4.c

---
+++ set2/4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a,b={1-6},c;
+if(b<=c)
+printf(" \n Enter The Numbers");
+scanf("%d",&a);
+switch(a)
+printf(" \n The number between the intervel");
+scanf("%d",&b);
+printf(" \n The number is odd");
+scanf("%d",&c);
+return 0;
+}
