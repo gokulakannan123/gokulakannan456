62438cee04dbbffcfbe81e9529cd85a775a8b95d
gokulakannan123 <35687945+gokulakannan123@users.noreply.github.com>
2018-02-07 14:14:32 +0530

    Create 2.c

---
+++ set2/2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a,b;
+printf(" \n Enter the palindromes");
+if(a>b)
+printf(" \n The number is palindrome ");
+scanf("%d",&a);
+if(a<b)
+printf(" \n The number is not palindrome  ");
+scanf("%d",&b);
+return 0;
+}
