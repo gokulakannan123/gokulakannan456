cf924ac0498bbe8f09b4fe3be98263b751db457f
gokulakannan123 <35687945+gokulakannan123@users.noreply.github.com>
2018-02-07 14:46:53 +0530

    Create 3.c

---
+++ set2/3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int a[5],b={2,3,5,7,11};
+if(a==b)
+printf(" \n The Number is Prime Number");
+scanf("%d",&a);
+printf(" \n The Number Is Not Prime Number");
+scanf("%d",b);
+return 0;
+}
