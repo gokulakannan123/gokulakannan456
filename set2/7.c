b439aabe4ac2813bdb6cc0253e3e83313e0df59b
gokulakannan123 <35687945+gokulakannan123@users.noreply.github.com>
2018-02-09 16:20:08 +0530

    Create 7.c

---
+++ set2/7.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{  
+int count,n; 
+printf("\n Even Numbers Between Two Intervals");  
+for(count = 1; count <= 10; count++) 
+{  
+if(count%2 ==0)
+{ 
+printf("%d ",count); 
+printf("\n Even Number Between Two Interval Is ");
+}  
+}  
+return 0;  
+} 
