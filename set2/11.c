5f0889bea7d1de62247752a61e7704b8686cf29c
gokulakannan123 <35687945+gokulakannan123@users.noreply.github.com>
2018-02-12 13:29:10 +0530

    Create 11.c

---
+++ set2/11.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+int n,fac=1,i;
+printf("Enter a number: ");
+scanf("%d", &n);
+if(n<0)
+printf("not valid");
+else
+{
+for(i=1;i<=n;++i)
+{
+fac=fac*i;
+}
+printf("factorial is:%d",fac);
+}
+return 0;
+}
