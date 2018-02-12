b80d55d28621d311a121c0963b88c55828f6fbc3
gokulakannan123 <35687945+gokulakannan123@users.noreply.github.com>
2018-02-12 13:42:31 +0530

    Create 12.c

---
+++ set2/12.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+int a,b,c,d,e,f;
+printf(" \n Enter the value");
+scanf("%d",&a);
+b=1*a;
+c=2*a;
+d=3*a;
+e=4*a;
+f=5*a;
+printf("%d\t%d\t%d\t%d\t%d\t",b,c,d,e,f);
+return 0;
+}
+
