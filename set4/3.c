e3f2f68261cb039d799f9bccbf9b97bc967eca37
gokulakannan123 <35687945+gokulakannan123@users.noreply.github.com>
2018-02-19 14:24:22 +0530

    Create 3.c

---
+++ set4/3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main()
+{
+char str[10];
+int i,count=1;
+printf("\n Enter The Sentence:");
+scanf("%[^\n]s",str);
+for(i=0;str[i]!='\0';i++)
+{
+count=count+1;
+}
+for(i=0;str[i]!='\0';i++)
+{
+if(str[i]==' ')
+{
+count=count+1;
+}
+}
+printf("\n The Number Of Space In The Line are:%d",count);
+return 0;
+}
