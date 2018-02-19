a80ce09d032374b39ec355bcf325217f3bed57d3
gokulakannan123 <35687945+gokulakannan123@users.noreply.github.com>
2018-02-19 13:58:49 +0530

    Create 8.c

---
+++ set3/8.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a[3],b,c,d;
+    printf("\n Enter The Numbers");
+    switch(a)
+    {
+    case 1:
+    printf("\n 2 3 :");
+    scanf("%d",&b);
+    break;
+    case 2:
+    printf("\n 5 7 :");
+    scanf("%d",&c);
+    break;
+    case 3:
+    printf("\n 2 9 :");
+    scanf("%d",&d);
+    break;
+    printf("\n The Values is");
+    }
+}
