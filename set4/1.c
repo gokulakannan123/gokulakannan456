cb9d2ce30ded00e4e8cabf3979727e39d8a1cd74
gokulakannan123 <35687945+gokulakannan123@users.noreply.github.com>
2018-02-19 14:11:26 +0530

    Create 1.c

---
+++ set4/1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main()
+{
+char str[50];
+int i,count=0;
+printf("\n Enter The Sentence:");
+scanf("%[^\n]s",str);
+for(i=0;str[i]!='\0';i++)
+{
+count=count+1;
+}
+for(i=0;str[i]!='\0';i++)
+{
+if(str[i]==' ')
+{
+count=count-1;
+}
+}
+printf("\n The Number Of Characters In The Line are:%d",count);
+return 0;
+}
