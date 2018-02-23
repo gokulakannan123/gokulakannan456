d61cd8c46fa9b4aeedbd32f712ffb833e4e7445b
gokulakannan123 <35687945+gokulakannan123@users.noreply.github.com>
2018-02-23 16:04:09 +0530

    Create 4.c

---
+++ set5/4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+int n;
+printf(" \n Enter The Number:");
+scanf("%d",&n);
+if(n>=1 && n<=10)
+{
+printf("\n Yes");
+}
+else
+{
+printf("\n No");
+}
+return 0;
+}
