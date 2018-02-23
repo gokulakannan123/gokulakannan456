dd5c7d905f0903ec0f9d926a7a4fb04379cf464f
gokulakannan123 <35687945+gokulakannan123@users.noreply.github.com>
2018-02-23 15:40:52 +0530

    Create 1.c

---
+++ set5/1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+char s[50];
+int k,i;
+printf(" \n Enter The String:");
+scanf("%s",s);
+printf(" \n Enter The Number:");
+scanf("%d",&k);
+for(i=0;i<k;i++)
+{
+printf("\n%s",s);
+}
+return 0;
+}
