33774c320df8414c041525dba38f4717baa4cf47
gokulakannan123 <35687945+gokulakannan123@users.noreply.github.com>
2018-02-26 14:01:15 +0530

    Create 1.c

---
+++ set6/1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c,d,e,f;
+    printf("\n Enter The Numbers");
+    scanf("%d%d%d",&a,&b,&c);
+    d=a;
+    e=b;
+    f=c;
+    printf("%d\n%d\n%d\n",d,e,f);
+    return 0;
+    
+}
