585021392f7c298bb7c777442039e28a5c9042f3
gokulakannan123 <35687945+gokulakannan123@users.noreply.github.com>
2018-03-05 13:12:42 +0530

    Create 4.c

---
+++ set7/4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("\n Enter the numbers");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    if(c%2==0)
+    {
+        printf("\n even");
+    }
+    else
+    {
+        printf("\n odd");
+    }
+    getch();
+}
+
