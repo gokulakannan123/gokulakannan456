cc7f8e5b7fb9c9aee4d30980db9905fc231a5d92
gokulakannan123 <35687945+gokulakannan123@users.noreply.github.com>
2018-03-14 16:25:59 +0530

    Create 1.c

---
+++ set9/1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	int n1,n2,i;
+	printf("\n Enter The Two Nmbers:");
+	scanf("%d %d",&n1,&n2);
+	for(i=1;i<=1;i++)
+	{
+	if(n1>n2)
+	{
+		printf("\n%d",n1-n2);
+	}
+	else
+	{
+		printf("\n%d",n2-n1);
+	}
+	}
+	return 0;
+}
