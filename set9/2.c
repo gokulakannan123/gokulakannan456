65e45a006ff313555be3357caebed3288b8d7bec
gokulakannan123 <35687945+gokulakannan123@users.noreply.github.com>
2018-03-14 16:39:39 +0530

    Create 2.c

---
+++ set9/2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main()
+{
+	float l,b,a;
+	printf(" \n Enter The Length and Breadth Values of The Farm:");
+	scanf("%f %f",&l,&b);
+	a=l*b;
+	printf("\n%0.5f",a);
+	return 0;
+}
