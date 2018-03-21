125c75ee32d267797c09017207eaf34bf4a06d31
gokulakannan123 <35687945+gokulakannan123@users.noreply.github.com>
2018-03-21 16:05:04 +0530

    Create 3.c

---
+++ set10/3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main()
+{	
+int a,b;
+printf("\n Enter The Temperature Value in Cel  ");
+scanf("%d",&a);
+b=a+273;
+printf("\n The Temperature Value in Kel  %d",b);
+return 0;
+}
